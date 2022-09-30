58f3d4f520538812fb082773f771276eac7adf34
ERi <erick_dr_17@hotmail.com>
2022-09-30 16:27:47 -0300

    inicial
    inicio del proyecto

---
+++ src/main/java/com/daniele/pbarra/PbarraApplication.java
@@ -0,0 +1,13 @@
+package com.daniele.pbarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbarraApplication.class, args);
+	}
+
+}
