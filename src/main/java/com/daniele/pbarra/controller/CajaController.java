c08c9be05c83cd9b355f006006d78b329a2f3cbf
ERi <erick_dr_17@hotmail.com>
2022-11-17 08:31:46 -0300

    inicio (7)
    CRUD en todos los elementos

--- src/main/java/com/daniele/pbarra/controller/CajaController.java
+++ src/main/java/com/daniele/pbarra/controller/CajaController.java
@@ -1,8 +1,75 @@
 package com.daniele.pbarra.controller;
 
-import org.springframework.stereotype.Controller;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniele.pbarra.model.Caja;
+import com.daniele.pbarra.service.CajaService;
+
+@RestController
+@RequestMapping(value = "/caja")
 public class CajaController {
+	@Autowired
+	private CajaService cajaserv;
+	
+	//trae la ruta de la imagen
+	private @Value("${ruta.template.plantillas}") String rutaTemplates;
+	
+	@GetMapping("/findall")
+	public ResponseEntity<List<Caja>> findAll(){
+		return new ResponseEntity<>(cajaserv.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Caja> findById(
+			@PathVariable("id") Integer id
+			) {
+		return new ResponseEntity<Caja>(cajaserv.findById(id), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity save(@RequestBody Caja e) throws URISyntaxException{
+		Caja caja = cajaserv.save(e);
+		return ResponseEntity.created(new URI("/caja/" + caja.getId())).body(caja);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteCaja(
+			@PathVariable("id") Integer id
+			){
+		cajaserv.delete(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/gettemplate", method = RequestMethod.GET, headers = "Accept=*/*")
+	@ResponseBody
+	public FileSystemResource createTemplate() {
+		System.out.print("Llego!");
+		//revisa ruta del archivo template
+		File target = new File(rutaTemplates + "fondo_blanco.png");
+		if(target.exists()) {
+			System.out.print("Existe");
+		} else {
+			System.out.print("No existe");
+		}
+		return null;
+	}
 
 }
--- src/main/java/com/daniele/pbarra/controller/SucursalController.java
+++ src/main/java/com/daniele/pbarra/controller/SucursalController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,20 +26,35 @@ public class SucursalController {
 	@Autowired
 	private SucursalService sucserv;
 	
-	@GetMapping("findall")
+	@GetMapping("/findall")
 	public ResponseEntity<List<Sucursal>> findAll() {
 		return new ResponseEntity<>(sucserv.findAll(), HttpStatus.OK);
 	}
 	
-	@GetMapping({"/{id}"})
-	public Sucursal findById(
+	@GetMapping("/{id}")
+	public ResponseEntity<Sucursal> findById(
 			@PathVariable("id") Integer id) {
 		System.out.print("prueba");
-		return sucserv.findById(id);
+		return new ResponseEntity<Sucursal>(sucserv.findById(id), HttpStatus.OK);
 	}
 	
-	@PostMapping(value = "save")
+	@PostMapping(value = "/save")
 	public @ResponseBody Sucursal save(@RequestBody Sucursal e) {
 		return sucserv.save(e);
 	}
+	
+	@PutMapping
+	public ResponseEntity<Sucursal> modificar(@RequestBody Sucursal sucursal){
+		Sucursal suc = sucserv.update(sucursal);
+		return new ResponseEntity<Sucursal>(suc, HttpStatus.OK);
+	}
+	
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteSucursal(@PathVariable("id") Integer id) throws Exception {
+		sucserv.delete(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
 }
