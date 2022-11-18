10c88941e71205a145aeb81023851567648f908c
ERi <erick_dr_17@hotmail.com>
2022-11-18 18:22:25 -0300

    implementada codigo de barra
    se implemento codigo de barra con info fija, falta optimizar  template el codigo de barra sale cortado en el

--- src/main/java/com/daniele/pbarra/controller/CajaController.java
+++ src/main/java/com/daniele/pbarra/controller/CajaController.java
@@ -1,10 +1,17 @@
 package com.daniele.pbarra.controller;
 
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
+import org.krysalis.barcode4j.impl.upcean.EAN13Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -31,6 +38,8 @@ public class CajaController {
 	
 	//trae la ruta de la imagen
 	private @Value("${ruta.template.plantillas}") String rutaTemplates;
+	//trae la ruta de destino
+	private @Value("${ruta.template.destino}") String rutadestino;
 	
 	@GetMapping("/findall")
 	public ResponseEntity<List<Caja>> findAll(){
@@ -64,12 +73,56 @@ public class CajaController {
 		System.out.print("Llego!");
 		//revisa ruta del archivo template
 		File target = new File(rutaTemplates + "fondo_blanco.png");
+		String codigoDeBarra = "";
+		BufferedImage algo = null;
 		if(target.exists()) {
 			System.out.print("Existe");
 		} else {
 			System.out.print("No existe");
 		}
+		try {
+			algo = generateEAN13BarcodeImage("123456789012");
+			//ImageIO.write(algo, "png", target); rutadestino
+			File rutaDestino = new File(rutadestino + "codigo_barra.png");
+			ImageIO.write(algo, "png", rutaDestino);
+			//lee la imagen recien creada
+			BufferedImage imagenbarra = ImageIO.read(rutaDestino);
+			//lee el fondo, template en el cual se dejara la imagen
+			BufferedImage imagenFondo = ImageIO.read(new File(rutaTemplates + "fondo_blanco.png"));
+			
+			//se crea la imagen en la cual se juntara todo
+			BufferedImage imagenFinal = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
+			
+			//creo manipulador 2g
+			Graphics2D grap = imagenFinal.createGraphics();
+			//grap.setFont(new Font("Helvetica medium", Font.BOLD, 50));
+			//dibuja ambas imagenes en la imagen creada
+			grap.drawImage(imagenFondo, 0, 0, 1692, 1692, null);
+			grap.drawImage(imagenbarra, 45, 45, null);
+			
+			//crea la imagen en el computador
+			ImageIO.write(imagenFinal, "png", new File(rutadestino + "imagen_final.png"));
+			
+			//imagenFondo = ImageIO.read(target);
+			
+			//BufferedImage imagenbarra = ImageIO.read(new File(rutadestino + "codigoBarra.png"));
+			System.out.print(algo);
+		}catch (Exception e ) {
+			System.out.print("Error " + e.getMessage());
+		}
+		
+		
+		
 		return null;
+	}
+	
+	public static BufferedImage generateEAN13BarcodeImage(String barcodeText) {
+	    EAN13Bean barcodeGenerator = new EAN13Bean();
+	    BitmapCanvasProvider canvas = 
+	      new BitmapCanvasProvider(160, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+
+	    barcodeGenerator.generateBarcode(canvas, barcodeText);
+	    return canvas.getBufferedImage();
 	}
 
 }
